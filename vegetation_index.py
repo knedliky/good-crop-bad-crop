dcd5a427c98583781fce1b14f593752b29de48e8
Simon <simon.karumbi@gmail.com>
2021-11-24 20:43:17 +1100

    added pipfiles and started index module

---
+++ vegetation_index.py
@@ -0,0 +1,29 @@
+# Constants
+indexes = {
+    'TRUE': 'TCI',
+    'NIR': 'B01',
+    'BLUE': 'B02',
+    'GREEN': 'B03',
+    'RED': 'B04',
+    'RED_EDGE': 'B05',
+    'NIR': 'B08',
+}
+
+# Vegetation Index Functions
+def calculate_ndvi():
+    return (NIR - RED) / (NIR + RED)
+
+
+def calculate_gndvi():
+    return (NIR - GREEN) / (NIR + GREEN)
+
+
+def calculate_endvi():
+    return ((NIR + GREEN) - (2 * BLUE)) / ((NIR + GREEN) + (2 * BLUE)) 
+
+
+def calculate_ndre():
+    return (NIR - RED_EDGE) / (NIR + RED_EDGE)
+
+if __name__ == 'main':
+    print("This shouldn't run")
